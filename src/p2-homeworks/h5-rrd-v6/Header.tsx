81a5acdf065af750a185d12298b3783846fa6e33
YakovcevMark <yakovcevmark@gmail.com>
2023-10-29 20:21:25 +0300

    Trying to deploy

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,12 +1,33 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Pages";
+import styled from "styled-components";
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
-        </div>
+        <Navs>
+            <div><NavLink to={PATH.PRE_JUNIOR}>Pre Junior</NavLink></div>
+            <div><NavLink to={PATH.JUNIOR}>Junior</NavLink></div>
+            <div><NavLink to={PATH.JUNIOR_PLUS}>Junior+</NavLink></div>
+        </Navs>
     )
 }
+const Navs = styled.div`
+  & > div > a {
+
+    color: green;
+  }
+
+  & > div > a.active {
+    background: #535b64;
+    color: blueviolet;
+  }
+
+  display: grid;
+  width:300px;
+  grid-template-columns: 1fr 1fr 1fr;
+  
+
+`
 
 export default Header
--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 
+import Error404 from "../h5-rrd-v6/pages/Error404";
+import PreJunior from "../h5-rrd-v6/pages/PreJunior";
+
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
     // add paths
@@ -14,13 +17,13 @@ function Routes() {
             {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
             {/*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
             {/*<Route path={'/'} exact render={() => <Redirect to={PATH.PRE_JUNIOR}/>}/>*/}
-
-            {/*<Route path={PATH.PRE_JUNIOR} render={() => <PreJunior/>}/>*/}
-                // add routes
-
+            {/**/}
+            {/*<Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>*/}
+            {/*    // add routes*/}
+            {/**/}
             {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
             {/*<Route render={() => <Error404/>}/>*/}
-
+            {/**/}
             {/*</Switch>*/}
         </div>
     )
