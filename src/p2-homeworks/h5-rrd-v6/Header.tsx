54c074b4c8e073a9f52306445c1abbbd48674a6b
YakovcevMark <yakovcevmark@gmail.com>
2023-11-02 21:02:12 +0300

    Added some logic in HW 6

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -6,9 +6,10 @@ import styled from "styled-components";
 function Header() {
     return (
         <Navs>
-            <div><NavLink to={PATH.PRE_JUNIOR}>Pre Junior</NavLink></div>
-            <div><NavLink to={PATH.JUNIOR}>Junior</NavLink></div>
-            <div><NavLink to={PATH.JUNIOR_PLUS}>Junior+</NavLink></div>
+                <div><NavLink to={PATH.PRE_JUNIOR}>Pre Junior</NavLink></div>
+                <div><NavLink to={PATH.JUNIOR}>Junior</NavLink></div>
+                <div><NavLink to={PATH.JUNIOR_PLUS}>Junior+</NavLink></div>
+                <div>kek</div>
         </Navs>
     )
 }
@@ -22,11 +23,15 @@ const Navs = styled.div`
     background: #535b64;
     color: blueviolet;
   }
-
+  transition-duration: 500ms;
+  transform: translate(-300px);
   display: grid;
-  width:300px;
-  grid-template-columns: 1fr 1fr 1fr;
-  
+  width:400px;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  &:hover{
+    transition-duration: 500ms;
+    transform: translate(0);
+  }
 
 `
 
